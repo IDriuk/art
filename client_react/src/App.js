a61b769543e30ec359470c30c65d7d894eb286d6
Ivan Driuk <veytarn@gmail.com>
2020-07-08 21:30:12 +0300

    add login form

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -20,7 +20,36 @@ function App() {
         </div>
       </nav>
 
-      <div className="auth-block">{/* <router-outlet></router-outlet> */}</div>
+      <div className="auth-block">
+
+        <form className="card">
+          <div className="card-content">
+            <span className="card-title">Войти в систему</span>
+            <div className="input-field">
+              <input  id="email" type="email" />
+              <label for="email">Email:</label>
+              {/* <span className="helper-text red-text">
+                <span>Email не должен быть пустым</span>
+                <span>Введите корректный email адрес</span>
+              </span> */}
+            </div>
+            <div className="input-field">
+              <input id="password" type="password" />
+              <label for="password">Пароль:</label>
+              {/* <span className="helper-text red-text">
+                <span>Пароль не должен быть пустым</span>
+                <span>Пароль должен быть больше символов. Сейчас</span>
+              </span> */}
+            </div>
+          </div>
+          <div className="card-action">
+            <button type="submit" className="modal-action btn waves-effect">
+              Войти
+            </button>
+          </div>
+        </form>
+
+      </div>
     </>
   );
 }
