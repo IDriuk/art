640c0f05dc68e240998cd614335f0624b1820879
Ivan Driuk <veytarn@gmail.com>
2020-07-24 14:14:10 +0300

    add leetcode path sum III solution

---
+++ exersize/leetcode/path-sum-iii.js
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+var pathSum = function(root, sum) {
+    let count = 0
+    
+    function countPaths(node, prevSums) {
+        if (!node) return
+        const sums = prevSums.map( el => (el + node.val))
+        sums.push(node.val)
+        const equals = sums.filter( el => el == sum)
+        count += equals.length
+        countPaths(node.left, sums)
+        countPaths(node.right, sums)
+    }
+    
+    countPaths(root, []) 
+    
+    return count
+};
+
+/* 
+class Solution {
+    int count = 0;
+    int k;
+    HashMap<Integer, Integer> h = new HashMap();
+    
+    public void preorder(TreeNode node, int currSum) {
+        if (node == null)
+            return;
+        
+        // current prefix sum
+        currSum += node.val;
+
+        // here is the sum we're looking for
+        if (currSum == k)
+            count++;
+        
+        // number of times the curr_sum − k has occured already, 
+        // determines the number of times a path with sum k 
+        // has occured upto the current node
+        count += h.getOrDefault(currSum - k, 0);
+        
+        // add the current sum into hashmap
+        // to use it during the child nodes processing
+        h.put(currSum, h.getOrDefault(currSum, 0) + 1);
+
+        // process left subtree
+        preorder(node.left, currSum);
+        // process right subtree
+        preorder(node.right, currSum);
+
+        // remove the current sum from the hashmap
+        // in order not to use it during 
+        // the parallel subtree processing
+        h.put(currSum, h.get(currSum) - 1);
+    }    
+            
+    public int pathSum(TreeNode root, int sum) {
+        k = sum;
+        preorder(root, 0);
+        return count;
+    }
+}
+*/
