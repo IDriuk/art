ab700e23a78f919c6c236ba469476d142c65758a
Ivan Driuk <veytarn@gmail.com>
2020-07-28 11:02:33 +0300

    add fast and slow runners solution

--- exersize/leetcode/linked_list_cycle.js
+++ exersize/leetcode/linked_list_cycle.js
@@ -45,5 +45,19 @@ var hasCycle = function(head) {
 
 // two pointers, fast and slow runners
 /*
- 
+public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) {
+        return false;
+    }
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (slow != fast) {
+        if (fast == null || fast.next == null) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+}
 */
