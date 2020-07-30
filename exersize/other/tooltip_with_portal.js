13e18e3d572235d832d5ea81cb19b40498d242bc
Ivan Driuk <veytarn@gmail.com>
2020-07-30 13:10:26 +0300

    add tooltip with react portal test task

---
+++ exersize/other/tooltip_with_portal.js
@@ -0,0 +1,47 @@
+// DCS Front End Software Engineer Test (Rev. July 2020) (click to start)
+
+class Tooltip extends React.Component {
+
+  render() {
+    // Write your code here
+    return ReactDOM.createPortal(
+      <div>{this.props.text}</div>,
+      document.getElementById("tooltip")
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    text: ''
+  }
+
+  onDocumentClick = (event) => {
+    if (event.target.tagName === 'BUTTON') {
+      this.setState({ text: event.target.textContent })
+    }
+  }
+  
+  componentDidMount() {
+    document.addEventListener('click', this.onDocumentClick)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onDocumentClick)
+  }
+
+  render() {
+    return <div>
+      {this.props.children}
+      <Tooltip text={this.state.text}/>
+    </div>
+  }
+}
+
+document.body.innerHTML = "<div id='root'></div><div id='tooltip'></div>";
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App>
+  <button id="button1">First button</button>
+  <button id="button2">Second button</button>
+</App>, rootElement);
+document.getElementById("button2").click();
+setTimeout(() => console.log(document.body.innerHTML));
