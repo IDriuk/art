67a7550cc566fe77e945c7263c1407195f784287
Ivan Driuk <veytarn@gmail.com>
2020-08-07 09:52:22 +0300

    add leetcode valid anagram solution

---
+++ exercise/leetcode/valid_anagram.js
@@ -0,0 +1,30 @@
+// Valid Anagram
+// https://leetcode.com/problems/valid-anagram/submissions/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    return s.split('').sort().join('') == t.split('').sort().join('')
+};
+
+/* 
+public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+    int[] counter = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        counter[s.charAt(i) - 'a']++;
+        counter[t.charAt(i) - 'a']--;
+    }
+    for (int count : counter) {
+        if (count != 0) {
+            return false;
+        }
+    }
+    return true;
+} 
+*/
