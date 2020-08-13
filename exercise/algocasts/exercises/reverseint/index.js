555df7681bde707ec250a614ec06bd987e6b4214
Ivan Driuk <veytarn@gmail.com>
2020-08-13 10:59:11 +0300

    add test  solution for algocasts reverse int

--- exercise/algocasts/exercises/reverseint/index.js
+++ exercise/algocasts/exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if ( n <= 9 && n >= 0 ) return n
+  let sign = n > 0 ? '' : '-'
+  let str = '' + n
+  str = sign + str.replace('-', '').replace(/0+$/, '').split('').reverse().join('')
+  return parseInt(str)
+}
 
 module.exports = reverseInt;
