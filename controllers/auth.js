57e0f24622fbfa4b49a394065ef95f4e60d2958b
Ivan Driuk <veytarn@gmail.com>
2020-08-14 19:12:00 +0300

    made register controller little more dry

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,50 +8,35 @@ const errorHandler = require('../utils/errorHandler')
 module.exports.register = async function(req, res) {
   let token = ''
   const { email, password } = req.body
-  const candidate = await User.findOne({email: req.body.email})
+  
+  try {
+    let user = await User.findOne({email: req.body.email})
 
-  if (candidate) {
-    
-    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
-    if (passwordResult) {
-      
-      token = jwt.sign({
-        email: candidate.email,
-        userId: candidate._id
-      }, keys.jwt, {expiresIn: 60 * 60})
-
-      res.status(200).json({
-        token: `Bearer ${token}`
-      })
+    if (user) {
+      const isPasswordsMatch = bcrypt.compareSync(req.body.password, user.password)
+      if (!isPasswordsMatch) {
+        res.status(401).json({
+          message: 'Passwords not match'
+        })
+        return
+      }
     } else {
-      
-      res.status(401).json({
-        message: 'Passwords not match'
-      })
+      const salt = bcrypt.genSaltSync(10)
+      user = await User({
+        email,
+        password: bcrypt.hashSync(password, salt)
+      }).save()
     }
-  } else {
-    const salt = bcrypt.genSaltSync(10)
-    const user = new User({
-      email,
-      password: bcrypt.hashSync(password, salt)
+
+    token = jwt.sign({
+      email: user.email,
+      userId: user._id
+    }, keys.jwt, {expiresIn: 60 * 60 * 24})
+
+    res.status(200).json({
+      token: `Bearer ${token}`
     })
-
-    try {
-      await user.save()
-      const newUser = await User.findOne({email: req.body.email})
-
-      token = jwt.sign({
-        email: newUser.email,
-        userId: newUser._id
-      }, keys.jwt, {expiresIn: 60 * 60})
-
-      res.status(200).json({
-        token: `Bearer ${token}`
-      })
-      
-    } catch(e) {
-      errorHandler(res, e)
-    }
-
+  } catch(e) {
+    errorHandler(res, e)
   }
 }
