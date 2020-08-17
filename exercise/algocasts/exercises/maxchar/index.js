c33fcf1d7f6ff3e677ff5eed642942fcf46bc93c
Ivan Driuk <veytarn@gmail.com>
2020-08-17 13:03:51 +0300

    add code from lesson about max char

--- exercise/algocasts/exercises/maxchar/index.js
+++ exercise/algocasts/exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {}
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+
+  
+}
 
 module.exports = maxChar;
