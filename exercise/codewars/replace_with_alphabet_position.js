488b4804f3f2bc2a1cfcb1bcc09c2db9344a4e73
Ivan Driuk <veytarn@gmail.com>
2020-08-21 10:30:47 +0300

    add codewars replace with alphabet position

---
+++ exercise/codewars/replace_with_alphabet_position.js
@@ -0,0 +1,11 @@
+// Replace With Alphabet Position
+// https://www.codewars.com/kata/546f922b54af40e1e90001da/train/javascript
+
+function alphabetPosition(text) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split('')
+  return text
+    .toLowerCase()
+    .replace(/[^a-z]/g, '')
+    .replace(/[a-z]/g, m => `${alphabet.indexOf(m) + 1} `)
+    .trim()
+}
