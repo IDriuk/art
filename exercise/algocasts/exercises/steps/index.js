136a94ecb545202efc9b2be06816e14dec584e34
Ivan Driuk <veytarn@gmail.com>
2020-09-02 13:13:11 +0300

    add alg steps solution

--- exercise/algocasts/exercises/steps/index.js
+++ exercise/algocasts/exercises/steps/index.js
@@ -17,6 +17,19 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = ''
+    for (let column =0; column < n; column++ ) {
+      if (row >= column) {
+        stair += '#'
+      } else {
+        stair += ' '
+      }
+    }
+    console.log(stair)
+  }
+
+}
 
 module.exports = steps;
