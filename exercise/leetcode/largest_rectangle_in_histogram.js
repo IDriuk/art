9c85bf44fd0e8c6dfd9d2f9592e510885a224668
Ivan Driuk <veytarn@gmail.com>
2020-12-02 11:07:20 +0200

    java solution for largest rectangle in histogram

--- exercise/leetcode/largest_rectangle_in_histogram.js
+++ exercise/leetcode/largest_rectangle_in_histogram.js
@@ -30,6 +30,24 @@ var largestRectangleArea = function(heights) {
 };
 
 /*
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack < Integer > stack = new Stack < > ();
+        stack.push(-1);
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+            stack.push(i);
+        }
+        while (stack.peek() != -1)
+            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() -1));
+        return maxarea;
+    }
+}
+*/
+
+/*
 var largestRectangleArea = function(heights) {
   if (!heights.length) { return 0 }
   
