a8737b755a40e960c4971186b1d3ac2cdd28eb37
Ivan Driuk <veytarn@gmail.com>
2020-12-02 11:14:40 +0200

    clean master branch

--- client/src/config/index.js
+++
@@ -1,3 +0,0 @@
-export const isProd = process.env.NODE_ENV === 'production'
-export const apiUrl = `${isProd ? '' : 'http://localhost:5000'}/api`
-export const defaultVideoLink = `https://drive.google.com/uc?export=download&id=1QDvlHH6WjjkwU2oKSdvI4BZcrLwGF2S6`
--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const controller = require('../controllers/auth')
-const router = express.Router()
-
-// localhost:5000/api/auth/register
-router.post('/register', controller.register)
-
-
-module.exports = router
